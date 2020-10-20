f0b448d3beed6f99bb54ec52c41006cae5239b20
csjh1 <csjh1@DESKTOP-1A8PMUQ>
2020-10-20 13:02:40 +0900

    StateListener 데이터 false = 현 날짜로 추가 및 true = 수정

--- sakila/src/sakila/listener/StateListener.java
+++ sakila/src/sakila/listener/StateListener.java
@@ -1,30 +1,21 @@
+  
 package sakila.listener;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
-import sakila.service.StateService;
-
+import sakila.service.*;
+import sakila.vo.*;
 @WebListener
-public class StateListener implements ServletContextListener { // ServletContextListener 세션이 생성되는 것을 감시
-	private StateService stateService;
-    public StateListener() {
+public class StateListener implements HttpSessionListener {
+    private StateService stateService;
+	public StateListener() {}
+    public void sessionCreated(HttpSessionEvent se)  { 
+        if(se.getSession().isNew()) {
+        	stateService = new StateService();
+        	stateService.countState();
+        }
     }
-    public void sessionCreated(HttpSessionEvent se) { //세션이 생성된 시점에 실행
-		if(se.getSession().isNew()) { // isNew()새션이 죽은 것인지 아예 새로 받은 세션인지 판별 =>아예 새로 받은 세션일 때 실행
-			stateService = new StateService();
-			
-		}
-	}
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void sessionDestroyed(HttpSessionEvent se)  {}
 }
