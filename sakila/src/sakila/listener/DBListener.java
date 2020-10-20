69850168a74dd01c67585a4fd4786add3decfa1b
csjh1 <csjh1@DESKTOP-1A8PMUQ>
2020-10-20 11:23:41 +0900

    톰캣이 실행될 때 실행되는 리스너(Class.forName)

--- sakila/src/sakila/listener/DBListener.java
+++ sakila/src/sakila/listener/DBListener.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
-public class DBListener implements ServletContextListener {
+public class DBListener implements ServletContextListener { //ServletContextListener : ÅèÄ¹ÀÌ ÄÑÁú¶§ ½ÇÇà
 
     public DBListener() {
     	
