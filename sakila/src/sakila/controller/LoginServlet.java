98d427e529c9d5ca2b6764ca7210b1dd2aab481d
csjh1 <csjh1@DESKTOP-1A8PMUQ>
2020-10-21 15:20:09 +0900

    id/pw 아무것도 입력하지 않았을 때 에러 발생 -> 조건부여 및 session부여

--- sakila/src/sakila/controller/LoginServlet.java
+++ sakila/src/sakila/controller/LoginServlet.java
@@ -23,8 +23,11 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//System.out.println("loginservlet) doget 시작");
 		HttpSession session = request.getSession(); //세션이 없을 때 새로 생성, 있으면 세션 리턴
+		
 		if(session.getAttribute("loginStaff") != null) {//로그인 상태일 떄
-			response.sendRedirect(request.getContextPath()+"/auth/IndexServlet");  // auth폴더는 로그인 되어있는 회원만 접근 가능
+			System.out.println("loginservlet) 로그인 상태인가?");
+
+			response.sendRedirect(request.getContextPath()+"/IndexServlet");  // auth폴더는 로그인 되어있는 회원만 접근 가능  | 초기 코드 auth/IndexServlet
 			return;
 		}
 		//System.out.println("loginservlet) doget stateListener and 로그인 확인 이후");
@@ -48,15 +51,27 @@ public class LoginServlet extends HttpServlet {
 	//로그린 액션으로 이동
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doPost) loginAction 진입");
-		
+		int id = 0;
+		String pw = "";
 		staffService = new StaffService();
 		
 		//requset 잘받았는지 디버깅
 		System.out.println("doPost) request.getParameter('id')=> "+ request.getParameter("id"));
 		System.out.println("doPost) request.getParameter('pw')=> " + request.getParameter("pw"));
 		Staff staff = new Staff(); // request ...
-		staff.setStaffId(Integer.parseInt(request.getParameter("id")));
-		staff.setPassword(request.getParameter("pw"));
+		
+		//아무것도 입력안하고 로그인 했을 때 에러 방지
+		if(request.getParameter("id")== "" || request.getParameter("pw")== "" ) {
+			staff.setStaffId(id);
+			staff.setPassword(pw);
+		}else {
+			System.out.println("doPost) 들어감?");
+			id = Integer.parseInt(request.getParameter("id"));
+			pw = request.getParameter("pw");
+			staff.setStaffId(id);
+			staff.setPassword(request.getParameter("pw"));
+		}
+		
 		
 		Staff returnStaff = null;
 		returnStaff = staffService.getStaffIDPW(staff);
@@ -64,7 +79,10 @@ public class LoginServlet extends HttpServlet {
 		if(returnStaff != null) {
 			System.out.println("로그인 성공");
 			//session 담고
+			
 			HttpSession session = request.getSession();
+			session.setAttribute("loginStaff", returnStaff); //doGet에서 로그인 상태인지 아닌지 판단하기 위해
+			
 			request.setAttribute("staff", returnStaff);
 			// indexServlet 포워딩
 			request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
